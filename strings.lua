790dc45a0f07891a04a82c81bbdffee6704f40f5
Dylan7654 <dylan.kreuzinger@gmail.com>
2024-04-22 23:30:31 +0200

    189 911(100. Dispolok)

--- strings.lua
+++ strings.lua
@@ -37,14 +37,16 @@ return {
 		["br189_db_name"] = 		"BR 189 076 DB Cargo (2007)",
 		
 		["br189_mrce_name"] = 		"BR 189(ES 64 F4) 997 MRCE (2009)",
-		["br189_mrce_name"] = 		"BR 189(ES 64 F4) 993 MRCE - Dispolok Lackierung (2015)",
+		["br189_mrce2_name"] = 		"BR 189(ES 64 F4) 993 MRCE - Dispolok Lackierung (2015)",
 				
-		["br189_dispo_name"] = 		"BR 189(ES 64 F4) 908 Dispolok (2009)",
+		["br189_dispo_name"] = 		"BR 189(ES 64 F4) 908 Dispolok (2004)",
+		["br189_dispo2_name"] = 	"BR 189(ES 64 F4) 906 Dispolok - ÖBB (2004)",
+		["br189_dispo3_name"] = 	"BR 189(ES 64 F4) 911 Dispolok - 100. Dispolok (2004)",
 		
 		["br189_sbb_name"] = 		"BR 189(ES 64 F4) 290 SBB Cargo Int - Rastattless (2017)",
-		["br189_sbb2_name"] = 		"BR 189(ES 64 F4) 102 SBB Cargo International (2014)",
+		["br189_sbb2_name"] = 		"BR 189(ES 64 F4) 102 SBB Cargo Int (2014)",
 		["br189_sbb3_name"] = 		"BR 189(ES 64 F4) 115 SBB Cargo Int - Alpäzähmer (2016)",
-		["br189_sbb4_name"] = 		"BR 189(ES 64 F4) 096 SBB Cargo International (2023)",
+		["br189_sbb4_name"] = 		"BR 189(ES 64 F4) 096 SBB Cargo Int (2023)",
 				
 		["br189_akiem_name"] = 		"BR 189(ES 64 F4) 112 Akiem (2022)",
 				
