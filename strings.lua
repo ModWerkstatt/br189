fa787326f8ad56dba344d110cd425f59302e15d9
Dylan7654 <dylan.kreuzinger@gmail.com>
2024-04-28 13:55:06 +0200

    Update

--- strings.lua
+++ strings.lua
@@ -34,14 +34,22 @@ return {
 		["dc_br189"] = "BR 189(ES 64 F4) Gleichstromsystem aktivieren?",
 		["option_dc_br189_desc"] = "Soll die BR 189(ES 64 F4) für das Gleichstromsystem aktiviert werden?",
 
+		["br189_siemens_name"] = 	"BR 189 929 Siemens - Grenzenlose Mobilität (2007)",
+		
 		["br189_db_name"] = 		"BR 189 076 DB Cargo (2007)",
 		
 		["br189_mrce_name"] = 		"BR 189(ES 64 F4) 997 MRCE (2009)",
 		["br189_mrce2_name"] = 		"BR 189(ES 64 F4) 993 MRCE - Dispolok Lackierung (2015)",
 		["br189_mrce3_name"] = 		"BR 189(ES 64 F4) 211 MRCE - MRCE/Dispolok Logo (2009)",
 		["br189_mrce4_name"] = 		"BR 189(ES 64 F4) 152 MRCE - PKP Intercity (2010)",
-		["br189_mrce5_name"] = 		"BR 189(ES 64 F4) 993 MRCE - Dispolok Lackierung (2015)",
-		["br189_mrce6_name"] = 		"BR 189(ES 64 F4) 993 MRCE - Dispolok Lackierung (2015)",
+		["br189_mrce5_name"] = 		"BR 189(ES 64 F4) 152 MRCE - PKP Cargo (2012)",
+		["br189_mrce6_name"] = 		"BR 189(ES 64 F4) 924 MRCE - TX Logistik (2015)",
+		["br189_mrce7_name"] = 		"BR 189(ES 64 F4) 290 MRCE - ERS Railways (2011)",
+		["br189_mrce8_name"] = 		"BR 189(ES 64 F4) 111 MRCE - Captrain (2013)",
+		["br189_mrce9_name"] = 		"BR 189(ES 64 F4) 115 MRCE - ETCS Ready (2009)",
+		["br189_mrce10_name"] = 	"BR 189(ES 64 F4) 993 MRCE - Dispolok Lackierung (2015)",
+		["br189_mrce11_name"] = 	"BR 189(ES 64 F4) 993 MRCE - Dispolok Lackierung (2015)",
+		["br189_mrce12_name"] = 	"BR 189(ES 64 F4) 993 MRCE - Dispolok Lackierung (2015)",
 				
 		["br189_dispo_name"] = 		"BR 189(ES 64 F4) 908 Dispolok (2004)",
 		["br189_dispo2_name"] = 	"BR 189(ES 64 F4) 906 Dispolok - ÖBB (2004)",
@@ -53,8 +61,13 @@ return {
 		["br189_sbb2_name"] = 		"BR 189(ES 64 F4) 102 SBB Cargo Int (2014)",
 		["br189_sbb3_name"] = 		"BR 189(ES 64 F4) 115 SBB Cargo Int - Alpäzähmer (2016)",
 		["br189_sbb4_name"] = 		"BR 189(ES 64 F4) 096 SBB Cargo Int (2023)",
+		["br189_sbb5_name"] = 		"Re 474 096 SBB Cargo Int (2023)",
+		["br189_sbb6_name"] = 		"Re 474 096 SBB Cargo Int (2023)",
 				
 		["br189_akiem_name"] = 		"BR 189(ES 64 F4) 112 Akiem (2022)",
+		["br189_akiem2_name"] = 	"BR 189(ES 64 F4) 844 Akiem - MRCE Lackierung (2022)",
+		["br189_akiem3_name"] = 	"BR 189(ES 64 F4) 112 Akiem (2022)",
+		["br189_akiem4_name"] = 	"BR 189(ES 64 F4) 112 Akiem (2022)",
 				
 		["br189_beacon_name"] = 	"BR 189(ES 64 F4) 993 Beacon - Dispolok Lackierung (2023)",
 
